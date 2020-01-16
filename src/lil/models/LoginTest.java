2168910876860837d6cd302d8f44b102ea6d8df5
unknown <aliawidat1@gmail.com>
2020-01-17 01:56:21 +0200

    Adding Complains + Reports
    Added complains basic interface for adding a complain or viewing complains.
    Also added Reports class for dealing with Monthly,Quarter,Complain reports.

--- src/lil/models/LoginTest.java
+++ src/lil/models/LoginTest.java
@@ -1,24 +1,24 @@
-package src.lil.models;
-
-import static org.junit.Assert.*;
-
-import java.sql.SQLException;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import junit.framework.TestCase;
-import src.lil.Enums.Role;
-
-public class LoginTest extends TestCase {
-
-	@Test
-	public void test() throws SQLException {
-		Login new_login = new Login();
-		Assert.assertTrue(new_login.check_user(4444, "4066")== Role.Client);
-		Assert.assertTrue(new_login.check_user(4444, "6")== Role.Unregistered);
-		Assert.assertTrue(new_login.check_user(31, "")==Role.Employee);
-		Assert.assertTrue(new_login.check_user(1, "")==Role.ChainManger);
-	}
-
-}
+//package src.lil.models;
+//
+//import static org.junit.Assert.*;
+//
+//import java.sql.SQLException;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import junit.framework.TestCase;
+//import src.lil.Enums.Role;
+//
+//public class LoginTest extends TestCase {
+//
+//	@Test
+//	public void test() throws SQLException {
+//		Login new_login = new Login();
+//		Assert.assertTrue(new_login.check_user(4444, "4066")== Role.Client);
+//		Assert.assertTrue(new_login.check_user(4444, "6")== Role.Unregistered);
+//		Assert.assertTrue(new_login.check_user(31, "")==Role.Employee);
+//		Assert.assertTrue(new_login.check_user(1, "")==Role.ChainManger);
+//	}
+//
+//}
--- src/lil/models/Report.java
+++ src/lil/models/Report.java
@@ -1,9 +1,7 @@
 package src.lil.models;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
@@ -17,28 +15,16 @@ import src.lil.controllers.ReportInterface;
 import src.lil.models.Order.NotFound;
 
 public class Report implements ReportInterface {
-	private File incomeReport,ordersReport,quarterReport,complainsReport;
+	private File monthlyReport,quarterReport,complainsReport;
 	
 	public Report() {
 		
 	}
 	
-	public Report(File report, Enum reportType) {
-		if(reportType.equals(src.lil.Enums.ReportType.OrdersReport)) {
-			ordersReport=report;
-		}else if(reportType.equals(src.lil.Enums.ReportType.ComplainsReport)) {
-			complainsReport = report; 
-		}else if(reportType.equals(src.lil.Enums.ReportType.QuarterlyEarningsReport)) {
-			quarterReport=report;
-		}else {
-			System.out.println("Enum Unkown");
-		}
-	}
 	public void prepareMonthlyReport() throws SQLException, NotFound, IOException{
 		
 		String sql= null;
 		Date date;
-		Date current_date = new Date(0);
 		String pattern = "MM/dd/yyyy HH:mm:ss";
 
 		// Create an instance of SimpleDateFormat used for formatting 
@@ -47,14 +33,15 @@ public class Report implements ReportInterface {
 		int order_id;
 		String item,price_Domain,delivery_location,dateAsString;
 //		Open a text file and create an output stream to Store the income data  
-		File targetFile = new File("src/lil/models/reports/MonthlyReports/incomeReport" + df.format(current_date) +".txt");
+		File targetFile = new File("src\\lil\\models\\reports\\MonthlyReports\\MonthlyReport.txt");
+		targetFile.createNewFile();
 		FileWriter fileWriter = new FileWriter(targetFile);
 
 //	    Read Orders IDs and the amount of $ paid from the result set
 	    try (Connection conn = DBConnection.getInstance().getConnection()){
 	    	  try {
 	    		  Statement stmt = conn.createStatement();
-	    		  sql = "SELECT * FROM orders.*";
+	    		  sql = "SELECT * FROM orders";
 	    		  ResultSet rs = stmt.executeQuery(sql);
 	    		  fileWriter.write("Order date ----- " + "Order id ----- Item ----- Total Cost ----- Delivery Location\n");
 	    		  while(rs.next()) {
@@ -64,7 +51,7 @@ public class Report implements ReportInterface {
 	    			  dateAsString = df.format(date);
 	    			  price_Domain = rs.getString("price_Domain");
 	    			  delivery_location= rs.getString("delivery_location");
-	    			  String finalOrderInfo = dateAsString + " " +order_id + " " + item + " " + price_Domain + " " + delivery_location + "\n";
+	    			  String finalOrderInfo = " " + dateAsString + " " +order_id + " " + item + " " + price_Domain + " " + delivery_location + "\n";
 	    			  try {
 	    			  fileWriter.write(finalOrderInfo);
 	    			  } catch (IOException e) {
@@ -72,7 +59,11 @@ public class Report implements ReportInterface {
 	    				  fileWriter.write(e.toString());
 					}
 	    		  }
-	    		  this.ordersReport=targetFile;
+    			  fileWriter.close();
+    			  rs.close();
+    			  stmt.close();
+    			  conn.close();
+	    		  this.monthlyReport=targetFile;
 	            }catch(SQLException se) {
 	            	fileWriter.write(se.toString());
 	            	fileWriter.write("SQLException: ");
@@ -97,21 +88,21 @@ public class Report implements ReportInterface {
 		
 		String sql= null;
 		Date date;
-		Date current_date = new Date(0);
 		String pattern = "MM/dd/yyyy HH:mm:ss";
 
 		// Create an instance of SimpleDateFormat used for formatting 
 		// the string representation of date according to the chosen pattern
 		DateFormat df = new SimpleDateFormat(pattern);
 		int complain_id;
-		String complain_title,complain_text,store_adress,dateAsString,contact_phone,contact_email;
+		String complain_title,complain_text,store_adress,dateAsString=null,contact_phone,contact_email;
 //		Open a text file and create an output stream to Store the income data  
-		File targetFile = new File("src/lil/models/reports/complainsReport" + df.format(current_date) + ".txt");
+		File targetFile = new File("C:\\Users\\ali\\git\\SoftwareEngeneering2020\\src\\lil\\models\\reports\\ComplainsReports\\complainsReport.txt");
+		targetFile.createNewFile();
 		FileWriter fileWriter = new FileWriter(targetFile);
-
+		
 //	    Read Orders IDs and the amount of $ paid from the result set
-	    try (Connection conn = DBConnection.getInstance().getConnection()){
-	    	  try {
+	    	 try {
+	    		  Connection conn = DBConnection.getInstance().getConnection();
 	    		  Statement stmt = conn.createStatement();
 	    		  sql = "SELECT * FROM complains";
 	    		  ResultSet rs = stmt.executeQuery(sql);
@@ -119,25 +110,27 @@ public class Report implements ReportInterface {
 	    			  complain_id = rs.getInt("complain_id");
 	    			  complain_title = rs.getString("complain_title");
 	    			  date = rs.getDate("complain_date");
-	    			  dateAsString = df.format(date);
+	    			  if(date!=null) 
+	    				  dateAsString = df.format(date);
 	    			  complain_text = rs.getString("complain_text");
 	    			  store_adress = rs.getString("store_adress");
 	    			  contact_email = rs.getString("contact_email");
 	    			  contact_phone = rs.getString("contact_phone");
 	    			  String finalOrderInfo = "Complain date: " + dateAsString + " Complain id: " + complain_id + " Complain title: " + complain_title + " Store adress: " + store_adress + " Phone: " + contact_phone + " Email: " + contact_email
-	    					  +"\n" + "Complain content: \n" + complain_text + "\n";
+	    					  +"\n" + "Complain content: " + complain_text + "\n";
 	    			  try {
 	    			  fileWriter.write(finalOrderInfo);
+	    			  fileWriter.write("||==================================||\n");
 	    			  } catch (IOException e) {
 						// TODO Auto-generated catch block
 	    				  fileWriter.write(e.toString());
-					     }
-	    			  fileWriter.close();
-	    			  rs.close();
-	    			  stmt.close();
-	    			  conn.close();
+					  }
 	    		  }
-	    		  this.ordersReport=targetFile;
+	    		  this.monthlyReport=targetFile;
+    			  fileWriter.close();
+    			  rs.close();
+    			  stmt.close();
+    			  conn.close();	 
 	            }catch(SQLException se) {
 	            	fileWriter.write(se.toString());
 	            	fileWriter.write("SQLException: ");
@@ -147,20 +140,24 @@ public class Report implements ReportInterface {
 	            	fileWriter.write("VendorError: ");
 	            	fileWriter.write(se.getErrorCode());
 	            }
-	    }catch(SQLException se) {
-        	fileWriter.write(se.toString());
-        	fileWriter.write("SQLException: ");
-        	fileWriter.write(se.getMessage());
-        	fileWriter.write("SQLState: ");
-        	fileWriter.write(se.getSQLState());
-        	fileWriter.write("VendorError: ");
-        	fileWriter.write(se.getErrorCode());
-        }
-	}
+
+	    }
+
 
 	public void prepareQuarterReport() throws SQLException, NotFound, IOException{
 		
-		
 	}
+	
+	
+	public File getMonthlyReport() {
+		return this.monthlyReport;
+	}
+	public File getQuarterReport() {
+		return this.quarterReport;
+	}
+	public File getComplainsReport() {
+		return this.complainsReport;
+	}
+	
 	
 }
