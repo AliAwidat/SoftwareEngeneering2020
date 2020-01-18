686051e07352f6d89543e0a41f9abc38a37db364
unknown <aliawidat1@gmail.com>
2020-01-18 14:22:02 +0200

    Fixed mail to send all reports
    Added functionality to send multipule reports needed.

--- src/lil/models/Report.java
+++ src/lil/models/Report.java
@@ -1,27 +1,32 @@
 package src.lil.models;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
+
+
 import src.lil.common.DBConnection;
 import src.lil.controllers.ReportInterface;
 import src.lil.models.Order.NotFound;
 
 public class Report implements ReportInterface {
 	private File monthlyReport,quarterReport,complainsReport;
-	
+	private Date lastUpdated = null;
 	public Report() {
 		
 	}
@@ -29,16 +34,18 @@ public class Report implements ReportInterface {
 	public void prepareMonthlyReport() throws SQLException, NotFound, IOException{
 		
 		String sql= null;
-		Date date;
-		String pattern = "MM/dd/yyyy HH:mm:ss";
+		Date date,report_date;
+		String pattern = "MMddyyyyHHmmss";
 
 		// Create an instance of SimpleDateFormat used for formatting 
 		// the string representation of date according to the chosen pattern
 		DateFormat df = new SimpleDateFormat(pattern);
 		int order_id;
 		String item,price_Domain,delivery_location,dateAsString;
+		String complain_title,complain_text,store_adress,contact_phone,contact_email;
+		String monthly_path="C:\\Users\\ali\\git\\SoftwareEngeneering2020\\src\\lil\\models\\reports\\MonthlyReports\\MonthlyReport"+df.format(new Date())+".txt";
 //		Open a text file and create an output stream to Store the income data  
-		File targetFile = new File("src\\lil\\models\\reports\\MonthlyReports\\MonthlyReport.txt");
+		File targetFile = new File(monthly_path);
 		targetFile.createNewFile();
 		FileWriter fileWriter = new FileWriter(targetFile);
 
@@ -64,6 +71,15 @@ public class Report implements ReportInterface {
 	    				  fileWriter.write(e.toString());
 					}
 	    		  }
+	    		    try {
+	    		    	fileWriter.write("\n||==================================||\n");
+	    		    	fileWriter.write("||===========Complains====Report======||\n");
+	    		    	fileWriter.write("||==================================||\n");
+	    		    	fileWriter.write(usingBufferedReader(this.complainsReport.toString()));
+	    		    } catch (IOException e) {
+	    		        // TODO Auto-generated catch block
+	    		        e.printStackTrace();
+	    		    }
     			  fileWriter.close();
     			  rs.close();
     			  stmt.close();
@@ -93,15 +109,17 @@ public class Report implements ReportInterface {
 		
 		String sql= null;
 		Date date;
-		String pattern = "MM/dd/yyyy HH:mm:ss";
+		String pattern = "MMddyyyyHHmmss";
 
 		// Create an instance of SimpleDateFormat used for formatting 
 		// the string representation of date according to the chosen pattern
 		DateFormat df = new SimpleDateFormat(pattern);
 		int complain_id;
 		String complain_title,complain_text,store_adress,dateAsString=null,contact_phone,contact_email;
+		String path="C:\\Users\\ali\\git\\SoftwareEngeneering2020\\src\\lil\\models\\reports\\ComplainsReports\\complainsReport"+df.format(new Date())+".txt";
 //		Open a text file and create an output stream to Store the income data  
-		File targetFile = new File("C:\\Users\\ali\\git\\SoftwareEngeneering2020\\src\\lil\\models\\reports\\ComplainsReports\\complainsReport.txt");
+		System.out.println(path);
+		File targetFile = new File(path);
 		targetFile.createNewFile();
 		FileWriter fileWriter = new FileWriter(targetFile);
 		
@@ -131,7 +149,7 @@ public class Report implements ReportInterface {
 	    				  fileWriter.write(e.toString());
 					  }
 	    		  }
-	    		  this.monthlyReport=targetFile;
+	    		  this.complainsReport=targetFile;
     			  fileWriter.close();
     			  rs.close();
     			  stmt.close();
@@ -150,7 +168,93 @@ public class Report implements ReportInterface {
 
 
 	public void prepareQuarterReport() throws SQLException, NotFound, IOException{
+
+		// Create an instance of SimpleDateFormat used for formatting 
+		// the string representation of date according to the chosen pattern    
+		String pattern = "MMddyyyyHHmmss";
+		DateFormat df = new SimpleDateFormat(pattern);
+		int current_complains_count=0,last_complains_count=5,current_total_income=0,last_total_income=400;
+		//Maps that contain the store name as a key, and as a value it contains either a certain store income/ratio 
+		Map current_stores_income=new HashMap(),last_stores_income=new HashMap(),stores_income_ratio=new HashMap();
+		double complains_ratio,total_income_ratio;
+		String sql= null,dateAsString=null;
 		
+		lastUpdated = new Date(Calendar.getInstance().getTime().getTime());
+		dateAsString=df.format(lastUpdated);
+		
+//		Open a text file and create an output stream to store income data  
+		File targetFile = new File("C:\\Users\\ali\\git\\SoftwareEngeneering2020\\src\\lil\\models\\reports\\QuarterReports\\quarterReport-"+df.format(new Date())+".txt");
+		targetFile.createNewFile();
+		FileWriter fileWriter = new FileWriter(targetFile);
+
+    	 try {
+    		  Connection conn = DBConnection.getInstance().getConnection();
+    		  Statement stmt = conn.createStatement();
+    		  sql = "SELECT * FROM complains";
+    		  ResultSet rs = stmt.executeQuery(sql);
+    		  while(rs.next()) {
+    			  current_complains_count++;
+    		  }
+    		  
+    		  sql = "SELECT * FROM prices";
+    		  rs = stmt.executeQuery(sql);
+    		  while(rs.next()) {
+    			  current_total_income+=rs.getInt(2);
+    			  int store_id=rs.getInt(1),income=rs.getInt(2);;
+    			  current_stores_income.put(store_id, income);
+    		  }
+
+    		  last_stores_income.put(1, 100);
+    		  last_stores_income.put(2, 250);
+    		  last_stores_income.put(3, 150);
+    		  
+    		  total_income_ratio=(double)current_total_income/last_total_income;
+    		  
+    		  stores_income_ratio.put(1, (double)200/100);
+    		  stores_income_ratio.put(2, (double)350/250);
+    		  stores_income_ratio.put(3, (double)250/150);
+    		  
+    		  complains_ratio=(double)current_complains_count/last_complains_count;
+
+
+			  try {
+				  fileWriter.write("||===============Quarter report for last 4 months. Date updated: "+ dateAsString+"====================||\n");
+				  fileWriter.write("||======================================================================================================||\n");
+				  fileWriter.write("||==================================Stores total monthly income:========================================||\n");
+				  Iterator iterator1 = current_stores_income.entrySet().iterator();
+				  Iterator iterator2 = last_stores_income.entrySet().iterator();
+				  Iterator iterator3 = stores_income_ratio.entrySet().iterator();
+			      while (iterator1.hasNext()) {
+				        Map.Entry pair1 = (Map.Entry)iterator1.next();
+				        Map.Entry pair2 = (Map.Entry)iterator2.next();
+				        Map.Entry pair3 = (Map.Entry)iterator3.next();
+				        fileWriter.write("Store_ID: "+pair1.getKey()+" || This month's income: "+pair1.getValue()+" || Last month's income: "+pair2.getValue()+" || Ratio: "+pair3.getValue()+"\n");
+			       }
+			      fileWriter.write("||======================================================================================================||\n");
+			      fileWriter.write("Total network income: "+current_total_income+" || Last month's total income: "+last_total_income+ " Ratio: "+total_income_ratio+"==================================||\n");
+				  fileWriter.write("||======================================================================================================||\n");
+				  fileWriter.write("||Complains count: "+ current_complains_count+" || Last month's complain count:"+last_complains_count+" || Complains Ratio: "+complains_ratio+"                           ||\n");
+				  fileWriter.write("||======================================================================================================||\n");
+				  
+			  } catch (IOException e) {
+				// TODO Auto-generated catch block
+				  fileWriter.write(e.toString());
+			  }
+    		  this.quarterReport=targetFile;
+			  fileWriter.close();
+			  rs.close();
+			  stmt.close();
+			  conn.close();	 
+            }catch(SQLException se) {
+            	fileWriter.write(se.toString());
+            	fileWriter.write("SQLException: ");
+            	fileWriter.write(se.getMessage());
+            	fileWriter.write("SQLState: ");
+            	fileWriter.write(se.getSQLState());
+            	fileWriter.write("VendorError: ");
+            	fileWriter.write(se.getErrorCode());
+            	fileWriter.close();
+            }
 	}
 	
 	
@@ -221,11 +325,52 @@ public class Report implements ReportInterface {
 	            ae.printStackTrace();
 	        }
 	   }
+	public void sendQuarterReport() {
+		  Properties props = System.getProperties();
+	        String host = "smtp.gmail.com";
+	        props.put("mail.smtp.starttls.enable", "true");
+	        props.put("mail.smtp.host", host);
+	        props.put("mail.smtp.user", "lilach.ltd");
+	        props.put("mail.smtp.password", "");
+	        props.put("mail.smtp.port", "587");
+	        props.put("mail.smtp.auth", "true");
+
+	        Session session = Session.getDefaultInstance(props);
+	        MimeMessage message = new MimeMessage(session);
+
+	        try {
+	            String from="lilach.ltd@gmail.com";
+	            String[] gmail2={"aliawidat1@gmail.com"};
+	            message.setFrom(new InternetAddress(from));
+	            InternetAddress[] toAddress = new InternetAddress[gmail2.length];
+
+	            // To get the array of addresses
+	            for( int i = 0; i < gmail2.length; i++ ) {
+	                toAddress[i] = new InternetAddress(gmail2[i]);
+	            }
+
+	            for (InternetAddress address : toAddress) {
+	                message.addRecipient(Message.RecipientType.TO, address);
+	            }
+
+	            message.setSubject("Network Quarter Report!");
+	            message.setText("Dear network manager, quarter report numbers are here!\n"+ usingBufferedReader(quarterReport.toString()));
+	            Transport transport = session.getTransport("smtp");
+	            transport.connect(host, "lilach.ltd@gmail.com", "umsrnjzmyvmkttyh");
+	            transport.sendMessage(message, message.getAllRecipients());
+	            transport.close();
+	        } catch (MessagingException ae) {
+	            ae.printStackTrace();
+	        }
+	   }
+	
 	
 	public static void main(String [] args) {
 		Report report= new Report();
 		try {
+			report.prepareComplainsReport();
 			report.prepareMonthlyReport();
+			report.prepareQuarterReport();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -237,5 +382,7 @@ public class Report implements ReportInterface {
 			e.printStackTrace();
 		}
 		report.sendStoreMonthlyReport();
+		report.sendQuarterReport();
     }
+
 }
