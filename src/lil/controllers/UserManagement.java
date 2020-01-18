af9c46105790dbf891ade4905dae2d32929d9647
miraqi132195 <49872544+miraqi132195@users.noreply.github.com>
2020-01-18 21:23:32 +0200

    ss
    ss

--- src/lil/controllers/UserManagement.java
+++ src/lil/controllers/UserManagement.java
@@ -1,17 +1,17 @@
 
 package src.lil.controllers;
 
-//import lil.Enums.Role;
-//import lil.models.Client;
-//import lil.models.Employee;
-//import lil.models.User;
+import lil.Enums.Role;
+import lil.models.Client;
+import lil.models.Employee;
+import lil.models.User;
 
 public interface UserManagement {
-//    Employee getEmployeeById(int userId);
-//    Client getClientById(int userId);
+    Employee getEmployeeById(int userId);
+    Client getClientById(int userId);
     boolean DeleteUser(int userId, boolean isClient);
     boolean updateUser(Object updatedUser);
     boolean setUserAbilityToOrder(int userId, boolean isBlock);
-//    boolean changeRule(int userId, Role role);
-//    boolean insertUser(User user);
+    boolean changeRule(int userId, Role role);
+    boolean insertUser(User user);
 }
