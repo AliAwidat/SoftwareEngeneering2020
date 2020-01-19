c0dea470bf18849e944a1cd352db1e8baae28866
orwah <orwah_wattad@hotmail.com>
2020-01-19 11:38:18 +0200

    yousef

--- src/lil/models/Item.java
+++ src/lil/models/Item.java
@@ -2,7 +2,7 @@ package src.lil.models;
 import src.lil.Enums.*;
 import src.lil.common.*;
 import src.lil.models.Order.AlreadyExists;
-
+import java.lang.String;
 import java.sql.*;
 
 public class Item {
@@ -10,7 +10,6 @@ public class Item {
 	private String dominantColor="",image;
 	private ItemType type;
 	private Double price;
-	
 	
 	public Item(ResultSet rs) throws SQLException{
 		super();
@@ -21,13 +20,14 @@ public class Item {
         this.id = rs.getInt("item_Id");
         this.dominantColor = rs.getString("dominant_color");
         this.type = ItemType.valueOf(rs.getString("item_type"));
-        this.price = rs.getDouble("item_price");
+        //this.price = rs.getDouble("item_price");
         this.updated = rs.getInt("updated");
         this.image = rs.getString("image");
+        
     }
-    public static Item findById(Integer id) throws SQLException {
+    public static Item findById(Integer id) throws SQLException, AlreadyExists {
         try (Connection db = DBConnection.getInstance().getConnection();
-             PreparedStatement preparedStatement = db.prepareStatement("select * from items where item_Id=?")) {
+             PreparedStatement preparedStatement = db.prepareStatement("select * from items where item_Id=?")){
             preparedStatement.setInt(1, id);
             try (ResultSet rs = preparedStatement.executeQuery()) {
                 if (!rs.next()) {
@@ -35,20 +35,78 @@ public class Item {
                 }
                 Item item = new Item(rs);
                 return item;
+            }catch (Exception e) {
+            	System.out.println(e.getMessage());
+                throw new AlreadyExists();
             }
+        } catch (Exception e) {
+        	System.out.println(e.getMessage());
+            throw new AlreadyExists();
         }
     }
+    //change price of one item in all stores
+    public static boolean changePriceAllStores(Integer itemId,Double newPrice) throws AlreadyExists {
+        try (Connection db = DBConnection.getInstance().getConnection();
+        	PreparedStatement preparedStatement2 = db.prepareStatement("update prices set price =price*"+newPrice+" where item_Id =?")){
+            preparedStatement2.setInt(1, itemId);
+            preparedStatement2.executeUpdate();
+            db.close();
+            return true;
+        } catch (Exception e) {
+           	System.out.println(e.getMessage());
+            throw new AlreadyExists();
+        }
+    }
+    //discount all items
+    public static boolean changePriceAllItems(Integer itemId,Double newPrice) throws AlreadyExists {
+        try (Connection db = DBConnection.getInstance().getConnection();
+        	PreparedStatement preparedStatement2 = db.prepareStatement("update prices set price =price*?")){
+            preparedStatement2.setDouble(1, newPrice);
+            preparedStatement2.setInt(2, itemId);
+            preparedStatement2.executeUpdate();
+            db.close();
+            return true;
+        } catch (Exception e) {
+           	System.out.println(e.getMessage());
+            throw new AlreadyExists();
+        }
+    }
+    //discount all items in one store
+    public static boolean changePricesInStore(Integer storeId,Double newPrice) throws AlreadyExists { // item id's "1,5,3,100"
+        try (Connection db = DBConnection.getInstance().getConnection();
+        	PreparedStatement preparedStatement2 = db.prepareStatement("update prices set price = price*? where store_id =?")){
+            preparedStatement2.setDouble(1, newPrice);
+            preparedStatement2.setInt(2, storeId);
+            preparedStatement2.executeUpdate();
+            db.close();
+            return true;
+        } catch (Exception e) {
+           	System.out.println(e.getMessage());
+            throw new AlreadyExists();
+        }
+    }
+    
     public static boolean delete(Integer id) throws SQLException, AlreadyExists {
         try (Connection db = DBConnection.getInstance().getConnection();
              PreparedStatement preparedStatement = db.prepareStatement("delete from items where item_Id = ?")) {
             preparedStatement.setInt(1, id);
         	preparedStatement.executeUpdate();
         	db.close();
-        	return true;
         } catch (Exception e) {
         	System.out.println(e.getMessage());
             throw new AlreadyExists();
         }
+        try (Connection db2 = DBConnection.getInstance().getConnection();
+        		PreparedStatement preparedStatement2 = db2.prepareStatement("delete from prices where item_Id = ?")) {
+               preparedStatement2.setInt(1, id);
+           	preparedStatement2.executeUpdate();
+           	db2.close();
+           	return true;
+        } catch (Exception e) {
+        	System.out.println(e.getMessage());
+        	throw new AlreadyExists();
+        }
+        
     }
     
     public boolean insert() throws SQLException, AlreadyExists{
@@ -61,19 +119,37 @@ public class Item {
         	preparedStatement.setInt(5, this.updated);
         	preparedStatement.executeUpdate();
         	db.close();
-        	return true;
         } catch (Exception e) {
         	System.out.println(e.getMessage());
             throw new AlreadyExists();
         }
-    }
-        
+        try (Connection db1 = DBConnection.getInstance().getConnection();
+        ResultSet rs= db1.prepareStatement("select max store_id from prices").executeQuery()){
+        try (Connection db = DBConnection.getInstance().getConnection();
+        ResultSet rs= db.prepareStatement("select distinct store_id from prices").executeQuery()){
+	        String list="";
+	        while(rs.next()) {
+	        	list += "("+rs.getInt(1)+","+rs.getInt(2)+","+rs.getInt(3)+"),";
+	        }
+	        list = list.substring(0, list.length()-1) + ";";
+	        try (Connection db2 = DBConnection.getInstance().getConnection();
+	        		PreparedStatement preparedStatement2 = db.prepareStatement("INSERT INTO prices(store_id, price, item_id) values"+list+"")) {
+	               preparedStatement2.setInt(1, id);
+	           	preparedStatement2.executeUpdate();
+	           	db2.close();
+	           	return true;
+	        } catch (Exception e) {
+	        	System.out.println(e.getMessage());
+	        	throw new AlreadyExists();
+	        }
+	    }
+    } 
     public static boolean update(Item item) throws SQLException, AlreadyExists{
         try (Connection db = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = db.prepareStatement("UPDATE items SET item_type=? ,dominant_color=? ,item_price=?, image=? ,updated=? WHERE item_Id=?")) {
+            PreparedStatement preparedStatement = db.prepareStatement("UPDATE items SET item_type=? ,dominant_color=? , image=? ,updated=? WHERE item_Id=?")) {
         	preparedStatement.setString(1, item.type.toString());
         	preparedStatement.setString(2, item.dominantColor);
-        	preparedStatement.setDouble(3, item.price);
+        	//preparedStatement.setDouble(3, item.price);
         	preparedStatement.setString(4, item.image);
         	preparedStatement.setInt(5, 1);
         	preparedStatement.setInt(6, item.id);
--- src/lil/models/ItemTest.java
+++ src/lil/models/ItemTest.java
@@ -7,32 +7,54 @@ import org.junit.jupiter.api.Test;
 import src.lil.Enums.ItemType;
 
 class ItemTest {
-	Item getFromDb = new Item(ItemType.BOUQUET,"",0.0);
+//	Item getFromDb = new Item(ItemType.BOUQUET,"",0.0);
 //	@Test
 //    public void addItem() throws Exception{
 //    	Item addToDb = new Item(ItemType.BOUQUET,"",20.0);
 //        if(!(addToDb.insert())) {System.out.println("Failed");}
 //    }
+//	@Test
+//    public void findItem() throws Exception{
+//    	Item getFromDb =  Item.findById(6);
+//        if(getFromDb==null) {System.out.println("Failed");}
+//    }
+//	@Test
+//    public void updateItem() throws Exception{
+////		getFromDb.setId(6);
+//		getFromDb.setPrice(50.0);
+////		getFromDb.setDominantColor("");
+////		getFromDb.setType(ItemType.BOUQUET);
+////		getFromDb.setUpdated(1);
+//    	boolean updated = Item.update(getFromDb);
+//        if(!(updated)) {System.out.println("Failed");}
+//    }
+//	@Test
+//    public void deleteItem() throws Exception{
+//    	boolean deleted = Item.delete(8);
+//        if(!(deleted)) {System.out.println("Failed");}
+//    }
+//	@Test
+//    public void findItem() throws Exception{
+//   	Item getFromDb =  Item.findById(2);
+//   	System.out.println(getFromDb.getType());
+//   	if(getFromDb==null) System.out.println("Failed");}
+//   	
+//	@Test
+//    public void changePriceAllStores() throws Exception{
+//		System.out.println(Item.changePriceAllStores(2, 0.8));
+//   	//System.out.println(getFromDb.getType());
+//   	//if(getFromDb==null) {System.out.println("Failed");}
+//  }
+//	@Test
+//    public void changePriceAllItems() throws Exception{
+//		System.out.println(Item.changePriceAllItems(1, 0.8));
+//   	//System.out.println(getFromDb.getType());
+//   	//if(getFromDb==null) {System.out.println("Failed");}
+//  }
 	@Test
-    public void findItem() throws Exception{
-    	Item getFromDb =  Item.findById(6);
-        if(getFromDb==null) {System.out.println("Failed");}
-    }
-	@Test
-    public void updateItem() throws Exception{
-//		getFromDb.setId(6);
-		getFromDb.setPrice(50.0);
-//		getFromDb.setDominantColor("");
-//		getFromDb.setType(ItemType.BOUQUET);
-//		getFromDb.setUpdated(1);
-    	boolean updated = Item.update(getFromDb);
-        if(!(updated)) {System.out.println("Failed");}
-    }
-	@Test
-    public void deleteItem() throws Exception{
-    	boolean deleted = Item.delete(8);
-        if(!(deleted)) {System.out.println("Failed");}
-    }
-
+    public void insettest() throws Exception{
+	Item newItem= new Item(ItemType.BOUQUET,"RED",100.0,"");
+	newItem.insert();
+	}
 }
 
