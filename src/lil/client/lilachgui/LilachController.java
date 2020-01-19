244f07ce2f5288b8db86dbd8cb02217ee165424c
unknown <aliawidat1@gmail.com>
2020-01-19 16:03:15 +0200

    Revert "Revert "Merge pull request #24 from AliAwidat/Ali""
    This reverts commit 1f33dbdca53283124a4e1f034fb8078a85b5c08a.

---
+++ src/lil/client/lilachgui/LilachController.java
@@ -0,0 +1,9 @@
+package src.lil.client.lilachgui;
+
+public class LilachController {
+
+	public LilachController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/lil/client/lilachgui/LilachMain.java
@@ -0,0 +1,23 @@
+package src.lil.client.lilachgui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LilachMain extends Application{
+
+	public LilachMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
