5d800be0260285c7eb4569aa12dfab524353e3ec
orwah <orwah_wattad@hotmail.com>
2020-01-25 18:58:30 +0200

    ITEM FIX

--- src/lil/models/Item.java
+++ src/lil/models/Item.java
@@ -10,13 +10,13 @@ import java.util.List;
 public class Item {
 	private int id,updated=0;
 	private String dominantColor="",image;
-	private ItemType type;
+	private static ItemType type;
 	private Double price;
 	private Boolean canAddToBouquet=false;
 	private List<Item> flowerInItem;
 	
 	public Item(ResultSet rs) throws SQLException{
-		super();
+		//super();
 		this.fillFieldsFromResultSet(rs);
 	}
 	
@@ -24,14 +24,36 @@ public class Item {
         this.id = rs.getInt("item_Id");
         this.dominantColor = rs.getString("dominant_color");
         this.type = ItemType.valueOf(rs.getString("item_type"));
-        this.price = getItemPrice(this.id);
+        //System.out.println("id= "+this.id);
+        this.price = rs.getDouble("item_price");
+       // System.out.println("price="+this.price);
         this.updated = rs.getInt("updated");
         this.image = rs.getString("image");
         this.updated=rs.getInt("updated");
-        this.flowerInItem=getFlowersInItemFromDb(this.id);
+        if(type==ItemType.CUSTOM) {
+        	this.flowerInItem=getFlowersInItemFromDb(this.id);
+        }else {
+        	this.flowerInItem=null;
+        }
         this.canAddToBouquet=rs.getBoolean("canAddToBouquet");
-
     }
+	
+//    public static boolean calcPriceOfCustomItem(Item item,int storeId) throws AlreadyExists { // item id's "1,5,3,100"
+//        try (Connection db = DBConnection.getInstance().getConnection();){
+//        	for(Item item:item.get)
+//	        	PreparedStatement preparedStatement2 = db.prepareStatement("select price from prices where store_id =? and ")
+//	            preparedStatement2.setInt(1, newPrice.intValue());
+//	            preparedStatement2.setInt(2, storeId);
+//	            preparedStatement2.executeUpdate();
+//	            db.close();
+//	            return true;
+//        } catch (Exception e) {
+//           	System.out.println(e.getMessage());
+//            throw new AlreadyExists();
+//        }
+//    }
+    
+	
     public static Item findById(Integer id) throws SQLException, AlreadyExists {
         try (Connection db = DBConnection.getInstance().getConnection();
              PreparedStatement preparedStatement = db.prepareStatement("select * from items where item_Id=?")){
@@ -135,6 +157,7 @@ public class Item {
 
            // throw new AlreadyExists();
         }
+        this.id=itemId;
         try (Connection db3 = DBConnection.getInstance().getConnection();
         ResultSet rs= db3.prepareStatement("select distinct store_id from prices").executeQuery()){
 	        while(rs.next()) {
@@ -149,6 +172,7 @@ public class Item {
         //	throw new AlreadyExists();
         }
 	        list = list.substring(0, list.length()-1) + ";";
+
 	        System.out.println(list);
 	        try (Connection db2 = DBConnection.getInstance().getConnection();
 	        		PreparedStatement preparedStatement2 = db2.prepareStatement("INSERT INTO prices(store_id, price, item_id) values"+list)) {
@@ -183,9 +207,12 @@ public class Item {
     // recieves item id and returns arrayList of all the flowers in it
     public static List<Item> getFlowersInItemFromDb(int itemId){
     	String flowers=getFlowersInItemIdFromDb(itemId);
+		System.out.println("flowers= "+flowers);
+		//String s="select * from items where item_Id in "+flowers;
+		//System.out.println(s);
     	List<Item> items=new ArrayList<Item>();
         try (Connection db = DBConnection.getInstance().getConnection();
-        		ResultSet rs = db.prepareStatement("select * from Items where item_id="+flowers).executeQuery()){
+        		ResultSet rs = db.prepareStatement("select * from items where item_Id in "+flowers).executeQuery()){
                 while (rs.next()) {
                     Item item = new Item(rs);
                 	items.add(item);
@@ -197,13 +224,14 @@ public class Item {
         return items;
     }
     
-    private  String itemsToString() {
+    private String itemsToString() {
     	String str="";
     	if(type==ItemType.CUSTOM) {
+    		str += "(";
 	    	for(Item item : flowerInItem) {
 	    		str+=item.getId()+",";
 	    	}
-	    	str = str.substring(0, str.length()-1) + ";";
+	    	str = str.substring(0, str.length()-1) + ")";
     	}
     	return str;
     }
@@ -215,6 +243,7 @@ public class Item {
                 if (rs.next()) {
                 	flowers = rs.getString(1);
                 }
+                
         } catch (Exception e) {
             	System.out.println(e.getMessage());
             	return null;
@@ -226,7 +255,8 @@ public class Item {
 //    	double totalPrice=0;
 //    	List<Item> items=new ArrayList<Item>();
 //    	items=getFlowersInItemFromDb(itemId);
-//        for (Item item : items) { 		      
+//        for (Item item : items) { 	
+//        	System.out.println(item.getId());
 //        	totalPrice+=item.getPrice();
 //        }
 //        return totalPrice;
@@ -234,20 +264,41 @@ public class Item {
     
     public double getItemPrice() {
     	double totalPrice=0;
-        for (Item item : flowerInItem) { 		      
-        	totalPrice+=item.getPrice();
-        }
+    	if(Item.getType()==ItemType.CUSTOM) {
+	        for (Item item : flowerInItem) { 		
+	        	totalPrice+=item.getPrice();
+	        }
+    	}else {
+    		totalPrice=price;
+    	}
         return totalPrice;
     }
     
     //filters
-    public static List<Item> filterItems(String type){
+    public static List<Item> filterItems(String type ,int storeId){
     	List<Item> items=new ArrayList<Item>();
-        try (Connection db = DBConnection.getInstance().getConnection();
-        		ResultSet rs = db.prepareStatement("select flowersInItem from Items where item_type="+type).executeQuery()){
-                if (rs.next()) {
-                    Item item = new Item(rs);
-                	items.add(item);                }
+        try (Connection db = DBConnection.getInstance().getConnection();){
+        		try(ResultSet rs = db.prepareStatement("SELECT * FROM items WHERE item_type"+type).executeQuery()){
+	                while (rs.next()) {
+	                    Item item = new Item(rs);
+	                	items.add(item);    
+	                }
+	            } catch (Exception e) {
+	            	  System.out.println(e.getMessage());
+	            	  System.out.println("fuck you not custom");
+	            	  return null;
+	            }for(Item item:items) {
+	            	int currId=item.getId();
+	        		try(ResultSet rs = db.prepareStatement("select price from prices where item_id="+currId+" and store_id="+storeId).executeQuery()){
+	        			if(rs.next()) {
+	        				item.setPrice(rs.getDouble(1));
+	        			}
+		            } catch (Exception e) {
+		            	  System.out.println(e.getMessage());
+		            	  System.out.println("FUCK PRICE");
+		            	  return null;
+		            }   
+	            }
         } catch (Exception e) {
             	System.out.println(e.getMessage());
             	return null;
@@ -267,6 +318,8 @@ public class Item {
 	    this.canAddToBouquet=canAddToBouquet;
 	    this.flowerInItem=new ArrayList<Item>();
     }
+    
+    
     
     
     public int getId() {
@@ -301,7 +354,7 @@ public class Item {
     	this.image = image;
     }
 
-    public ItemType getType() {
+    public static ItemType getType() {
     	return type;
     }
 
@@ -318,6 +371,7 @@ public class Item {
     }
     public void addItem(Item item) {
     	flowerInItem.add(item);
+    	price+=item.getPrice();
     }
     public void removeItem(Item item) {
          for (Item i : flowerInItem) { 		      
