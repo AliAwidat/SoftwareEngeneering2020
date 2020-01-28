541628514551a3aa3f04929d82719d59ecc93320
miraqi132195 <49872544+miraqi132195@users.noreply.github.com>
2020-01-28 21:27:02 +0200

    order
    order

--- src/lil/client/lilachgui/LilachController.java
+++ src/lil/client/lilachgui/LilachController.java
@@ -159,7 +159,7 @@ public abstract class LilachController {
 
 	@FXML
 	public void handle_my_order_butt(ActionEvent event) throws IOException {
-		get_scene("myOrdersPage.fxml", "My orders history");
+		get_scene("OrderHistory.fxml", "My orders history");
 	}
 
 	@FXML
